4165a44d92a97ea8291a7445dca6857e0f96819d
EmperorStarfinder <emperor@secondgalaxy.com>
2017-12-20 15:14:23 -0500

    Rebase Part 1
    Remove the 0.8.1 code base in favor of the last best known modular version for the base code.
    
    Signed-Off-By: EmperorStarfinder<emperor@secondgalaxy.com>

--- runprebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mono Prebuild.exe /target nant
-# needed until we break up OpenSim.exe
-perl -pi -e 's{OpenSim.dll}{OpenSim.exe}' OpenSim/ApplicationPlugins/LoadRegions/OpenSim.ApplicationPlugins.LoadRegions.dll.build
-mono Prebuild.exe /target monodev
-mono Prebuild.exe /target vs2008
