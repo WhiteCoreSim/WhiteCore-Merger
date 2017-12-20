2932a37b6fd60b9ff77304fe4cc177c4c820fddf
EmperorStarfinder <emperor@secondgalaxy.com>
2017-12-20 18:40:43 -0500

    Fix runprebuild.bat and .sh
    - Correct the directory to Prebuild.exe in both runprebuild2010 batch and shell scripts
    
    Signed-Off-By: EmperorStarfinder<emperor@secondgalaxy.com>

--- runprebuild2010.sh
+++ runprebuild2010.sh
@@ -4,28 +4,28 @@ case "$1" in
 
   'clean')
 
-    mono bin/Prebuild.exe /clean
+    mono Prebuild.exe /clean
 
   ;;
 
 
   'autoclean')
 
-    echo y|mono bin/Prebuild.exe /clean
+    echo y|mono Prebuild.exe /clean
 
   ;;
 
 
   'vs2010')
   
-    mono bin/Prebuild.exe /target vs2010
+    mono Prebuild.exe /target vs2010
   
   ;;
 
   *)
 
-    mono bin/Prebuild.exe /target nant
-    mono bin/Prebuild.exe /target vs2010
+    mono Prebuild.exe /target nant
+    mono Prebuild.exe /target vs2010
 
   ;;
 
